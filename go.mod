8b900a13998e6dc63df51c61db0f49d20bb0df80
Arnie97 <arnie97@gmail.com>
2021-11-19 17:11:47 +0800

    Implement dump-gocq

--- go.mod
+++ go.mod
@@ -1,10 +1,9 @@
-module github.com/mdawsonuk/LevelDBDumper
+module github.com/arnie97/dump-gocq
 
 go 1.16
 
 require (
+	github.com/Mrs4s/MiraiGo v0.0.0-20211118132817-19e205000410
 	github.com/gookit/color v1.3.8
-	github.com/hashicorp/go-version v1.2.1
 	github.com/syndtr/goleveldb v1.0.0
-	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2
 )
--- src/LevelDBDumper/LevelDBDumper.go
+++ src/LevelDBDumper/LevelDBDumper.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
 	"fmt"
 	"os"
-	"os/signal"
-	"strings"
-	"syscall"
 	"time"
 
-	"github.com/gookit/color"
+	"github.com/Mrs4s/MiraiGo/message"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/opt"
 )
 
-// VERSION of LevelDB Dumper
-const VERSION string = "3.0.0-beta.1"
+func dumpGoCQ(dbPath, jsonPath string) {
+	gob.Register(message.Sender{})
+
+	db, err := leveldb.OpenFile(dbPath, &opt.Options{
+		WriteBuffer: 128 * opt.KiB,
+	})
+	if err != nil {
+		checkError(err)
+		return
+	}
+
+	jsonFile, err := os.Create(os.Args[2])
+	if err != nil {
+		checkError(err)
+		return
+	}
+
+	for _, parsedDB := range parsedDatabases {
+		ret := make([]map[string]interface{}, 0, len(parsedDB.keys))
+		for _, k := range parsedDB.keys {
+			k := []byte(k)
+			v, err := db.Get(k, nil)
+			checkError(err)
+
+			var g map[string]interface{}
+			checkError(gob.NewDecoder(bytes.NewReader(v)).Decode(&g))
+			ret = append(ret, g)
+		}
+		encoder := json.NewEncoder(jsonFile)
+		encoder.SetIndent("", "\t")
+		checkError(encoder.Encode(ret))
+
+		fmt.Printf("%d messages written to %#v\n", len(ret), jsonPath)
+	}
+}
 
 var (
 	searchResult    []string
@@ -20,104 +55,26 @@ var (
 )
 
 func main() {
-	dumpDBs(os.Args)
-}
-
-func dumpDBs(args []string) {
-
-	getArgs(args)
-
-	if !noHeader {
-		fmt.Println()
-		fmt.Println(fmt.Sprintf("LevelDB Dumper %s", VERSION))
-		fmt.Println()
-		fmt.Println("Author: Matt Dawson")
-		fmt.Println()
-	}
-
-	if help {
-		printUsage()
-		os.Exit(0)
-	}
-
-	fmt.Println("Command Line:", strings.Join(args[1:], " "))
-	fmt.Println()
-
-	needsUpdate, latestVersion := checkUpdate(VERSION)
-
-	if !needsUpdate {
-		color.Magenta.Println("You are using the latest version of LevelDB Dumper")
-		if checkForUpdate {
-			os.Exit(0)
-		}
-	} else if checkForUpdate {
-		color.Cyan.Println(fmt.Sprintf("Version %s is now available for LevelDB Dumper - please update!", latestVersion))
-		os.Exit(0)
-	}
-
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		color.FgLightBlue.Println(fmt.Sprintf("Found %d results so far", len(searchResult)))
-		color.Red.Println("Ctrl+C detected, quitting...")
-		os.Exit(0)
-	}()
-
-	if rootPath == "" {
-		printUsage()
-		color.Red.Println("Missing -d argument")
+	if len(os.Args) != 3 {
+		fmt.Println("usage: dump-gocq <leveldb path> <json path>")
 		os.Exit(1)
 	}
 
-	switch strings.ToLower(outputType) {
-	case
-		"csv",
-		"json":
-		break
-	default:
-		color.Yellow.Println(fmt.Sprintf("%s is not a recognised output type. Defaulting to CSV", outputType))
-		fmt.Println()
-		outputType = "csv"
+	rootPath = os.Args[1]
+	jsonPath := os.Args[2]
+	if dbExists, err := fileExists(rootPath); !dbExists || err != nil {
+		checkError(err)
+		fmt.Printf(`The path "%s" doesn't exist`, rootPath)
+		return
 	}
-
-	dbPresent, _ := fileExists(rootPath)
-
-	if !dbPresent {
-		color.Red.Println(fmt.Sprintf("The path %s doesn't exist", rootPath))
-		os.Exit(2)
-	}
-
-	testFile, err := os.Open(rootPath)
-	if err != nil {
-		color.Yellow.Println(fmt.Sprintf("Unable to open %s - make sure you haven't escaped the path with \\\"", rootPath))
-		testFile.Close()
-		os.Exit(2)
-	}
-	testFile.Close()
-
-	if !isAdmin() {
-		color.Red.Println("You should run LevelDB Dumper with root/Administrator privileges")
-	} else {
-		color.FgLightBlue.Println("Running LevelDB Dumper with root/Administrator privileges")
-	}
-	fmt.Println()
 
 	start := time.Now()
 
-	// See LevelDBSearch.go
+	quiet = true
 	searchForDBs()
-	// See LevelDBParse.go
 	readDBs()
-	// See FileOutputs.go
-	writeDBInfo()
+	dumpGoCQ(rootPath, jsonPath)
 
 	elapsed := time.Now().Sub(start)
-	color.FgLightBlue.Println(fmt.Sprintf("Completed search in %v", elapsed))
-
-	if needsUpdate {
-		color.Magenta.Println(fmt.Sprintf("Version %s is now available for LevelDB Dumper - please update!", latestVersion))
-	}
-
-	os.Exit(0)
+	fmt.Printf("Completed in %v\n", elapsed)
 }
